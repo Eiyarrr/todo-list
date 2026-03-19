ae430aa6ffa2495446e11f55c53760e9b7a4e240
Eiyarrr <eiyar@eiyar.com>
2026-03-19 22:48:04 +0000

    Added enum for status and Task struct for tasks as well as an example task

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
+use chrono::{ Local, NaiveDate };
+
+enum Status {
+    NotStarted,
+    Started,
+    Completed,
+}
+
+struct Task {
+    name: String,
+    description: String,
+    due_date: NaiveDate,
+    status: Status,
+}
+
+impl Task {
+    fn get_status(&self) -> String {
+        match self.status {
+            Status::NotStarted => String::from("NS"),
+            Status::Started => String::from("S"),
+            Status::Completed => String::from("C"),
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    example();
+}
+
+fn example() {
+    let example_task = Task {
+        name: String::from("name"),
+        description: String::from("description"),
+        due_date: Local::now().date_naive(),
+        status: Status::NotStarted,
+    };
+
+    println!(
+        "{}, {}, {}, {}",
+        example_task.name,
+        example_task.description,
+        example_task.due_date.to_string(),
+        example_task.get_status()
+    );
 }
