7201d3ee9648e878a98b720f00e32dcf29abbf23
Eiyarrr <eiyar@eiyar.com>
2026-03-20 10:27:26 +0000

    Fully implemented Vec<Task> in add, remove, update, and print

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,22 @@
-use serde::{Deserialize, Serialize};
-use std::fs;
 use std::io;
 use std::process;
 
-#[derive(Serialize, Deserialize)]
 enum Status {
     NotStarted,
     Started,
     Completed,
 }
 
-#[derive(Serialize, Deserialize)]
+impl Status {
+    fn next_status(&self) -> Status {
+        match self {
+            Status::NotStarted => Status::Started,
+            Status::Started => Status::Completed,
+            Status::Completed => Status::Completed,
+        }
+    }
+}
+
 struct Task {
     name: String,
     description: String,
@@ -63,8 +69,17 @@ fn main() {
 }
 
 fn new_task(tasks: &mut Vec<Task>) {
-    let name: String = String::new();
-    let description: String = String::new();
+    clearscreen::clear().expect("Failed to clear console");
+    println!("What is the name of your new task?");
+    let mut name: String = String::new();
+    io::stdin().read_line(&mut name).expect("Invalid input!");
+    let name = name.trim().to_string();
+
+    println!("What is the description of your new task?");
+    let mut description: String = String::new();
+    io::stdin().read_line(&mut description).expect("Invalid input!");
+    let description = description.trim().to_string();
+
     let status: Status = Status::NotStarted;
     tasks.push(Task {
         name,
@@ -73,13 +88,34 @@ fn new_task(tasks: &mut Vec<Task>) {
     })
 }
 
-fn update_task(tasks: &mut Vec<Task>) {}
+fn update_task(tasks: &mut Vec<Task>) {
+    clearscreen::clear().expect("Failed to clear console");
+    println!("What task would you like to update?");
+    let mut input: String = String::new();
+    io::stdin().read_line(&mut input).expect("Invalid input!");
+    let input = input.trim();
+
+    if let Some(pos) = tasks.iter().position(|t| t.name == input) {
+        let previous_status = tasks[pos].get_status();
+        tasks[pos].status = tasks[pos].status.next_status();
+        println!(
+            "Updated task {} from {} to {}",
+            input,
+            previous_status,
+            tasks[pos].get_status()
+        );
+    } else {
+        println!("Invalid task");
+    }
+}
+
 fn remove_task(tasks: &mut Vec<Task>) {
     clearscreen::clear().expect("Failed to clear console");
     println!("What task would you like to remove?");
     let mut input: String = String::new();
     io::stdin().read_line(&mut input).expect("Invalid input!");
     let input = input.trim();
+
     if let Some(pos) = tasks.iter().position(|t| t.name == input) {
         tasks.remove(pos);
         println!("Removed task {}", input);
@@ -87,5 +123,14 @@ fn remove_task(tasks: &mut Vec<Task>) {
         println!("Invalid task");
     }
 }
+
 fn print_tasks(tasks: &mut Vec<Task>) {
+    for task in tasks {
+        println!(
+            "name: {}, description: {}, status: {}",
+            task.name,
+            task.description,
+            task.get_status()
+        );
+    }
 }
